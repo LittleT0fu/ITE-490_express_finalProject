8ef2e51346cd4451e63ca88815b682389be3458c
LittleT0fu <chinchanathavornkraiwit@gmail.com>
2023-02-05 17:35:21 +0700

    test gitignore

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send('ITE-490 Final Project');
+  res.send('ITE-490 Final Projects');
 });
 
 module.exports = router;
